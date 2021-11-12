68cee30eb5aa7b0f4ec0d21317e5d575c1304a64
tigleonid <tigleonid@icloud.com>
2021-11-12 16:13:54 +0300

    add lab_1

--- lab_1/lab_1.py
+++ lab_1/lab_1.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 
 if __name__ == '__main__':
     df = pd.read_csv("data_1.csv")
-    print(df["color"])
-    plt.hist(df[df["color"] == 1]["mass"], density=True, bins=30)
-    plt.ylabel('Probability')
-    plt.xlabel('Data')
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.gca()
+    df[df["color"] == 4].hist(ax=ax)
     plt.show()
